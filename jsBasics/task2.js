5bf72624d618015b52adf077db6ce05a8b34c86c
Anastasiia Latysheva <63902118+NastyaLatysh@users.noreply.github.com>
2024-07-10 22:30:46 +0300

    jsBasics homework (#3)

---
+++ jsBasics/task2.js
@@ -0,0 +1,7 @@
+let userName1 = 'Alice';
+let userName2 = 'Ben';
+let welcomeUsers = 'Welcome ' + userName1 + ' and ' + userName2 + '!';
+let welcomeUsersTemplate = `Welcome ${userName1} and ${userName2}!`;
+
+console.log(welcomeUsers);
+console.log(welcomeUsersTemplate);
---
+++ jsBasics/task3.js
@@ -0,0 +1,5 @@
+let userAge = 15;
+let isUserAdult = userAge >= 18;
+
+console.log('user age =', userAge);
+console.log('is user adult =', isUserAdult);
