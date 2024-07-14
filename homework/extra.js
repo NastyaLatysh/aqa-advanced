dae9c03b5d76f2dd77a52889c18590ce3a31008b
Anastasiia Latysheva <63902118+NastyaLatysh@users.noreply.github.com>
2024-07-14 13:13:28 +0300

    homework 5.1 (#4)
    * homework 5.1
    
    * added empty line
    
    * exta task
    
    ---------
    
    Co-authored-by: “Anastasia <“anastasiia.latysheva@socialtechnologies.io”>

---
+++ homework/extra.js
@@ -0,0 +1,9 @@
+let string = 'JHHsfdHH';
+let string2 = string.toUpperCase();
+
+ if (string === string2) {
+    console.log('all string upper case');
+ } else {
+    console.log('string have lower case');
+ }
+
