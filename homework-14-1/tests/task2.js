a15904925c5f490d0ad61a33e609fe19c0b7a94a
“Anastasia <anastasiia.latysheva@socialtechnologies.io>
2024-08-31 21:22:11 +0300

    Homework 14

---
+++ homework-14-1/tests/task2.js
@@ -0,0 +1,18 @@
+const axios = require("axios");
+const BaseController = require('../src/BaseController.js');
+
+//task 2, Testing Request Headers and Params
+
+
+test ('Testing Request Headers and Params', async () => {
+    let controller = new BaseController;
+    
+    try{
+        response = await controller.axiosInstance.get();
+        expect(response.config.headers).toHaveProperty('Content-Type', 'kitty');
+        expect(response.config.params).toHaveProperty('lolkek', 'chebureck');
+    } catch (err) {
+       console.log(err);
+    }
+
+});
