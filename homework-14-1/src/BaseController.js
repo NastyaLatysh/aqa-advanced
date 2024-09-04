b1ed6ce99a2876c5f1935f5beaf04d43398291ef
“Anastasia <anastasiia.latysheva@socialtechnologies.io>
2024-09-04 22:53:30 +0300

    fix fotmat file

--- homework-14-1/src/BaseController.js
+++ homework-14-1/src/BaseController.js
@@ -1,19 +1,17 @@
 const axios = require('axios');
 
 class BaseController {
-    constructor () {
-        this.axiosInstance = axios.create({
-            baseURL: 'https://jsonplaceholder.typicode.com',
-            headers: {
-                'Content-Type': 'kitty'
-            },
-            params: {
-                "lolkek": "chebureck" 
-              },
-        });
-    }
+	constructor() {
+		this.axiosInstance = axios.create({
+			baseURL: 'https://jsonplaceholder.typicode.com',
+			headers: {
+				'Content-Type': 'kitty',
+			},
+			params: {
+				lolkek: 'chebureck',
+			},
+		});
+	}
 }
-
-
 
 module.exports = BaseController;
